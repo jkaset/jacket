fff5608342614023f83770143a7bc3dc2b230bf6
Jasmin Kaset <kasetski@gmail.com>
2020-11-01 07:15:44 -0600

    created form in index.html

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -2,4 +2,9 @@ let weather = []
 
 export const getWeather = (zip) => {
   return fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${keys.weatherKey}`)
+  .then(response => response.json())
+  .then(parsedResponse => {
+    weather = parsedResponse
+  })
 }
+
